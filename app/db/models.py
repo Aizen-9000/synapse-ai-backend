3c05cab3cb88d06872916f05bbf76a04a2f144d7
Aizen-9000 <majianupam173@gmail.com>
2025-12-26 11:08:13 +0530

    l6

--- app/db/models.py
+++ app/db/models.py
@@ -1,8 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Identity
 from .base import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
+
+    id = Column(
+        Integer,
+        Identity(always=True),
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(String(255), unique=True, index=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
