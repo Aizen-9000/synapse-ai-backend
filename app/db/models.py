567c52544f038146b08616129c0ecfa24682d7b6
Aizen-9000 <majianupam173@gmail.com>
2025-12-26 10:42:24 +0530

    ggo

--- app/db/models.py
+++ app/db/models.py
@@ -3,6 +3,6 @@ from .base import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
