96f7e97fac9be4fb3e5354a34cf8558c405289c6
Aizen-9000 <majianupam173@gmail.com>
2025-12-26 13:02:12 +0530

    lau

--- app/db/models.py
+++ app/db/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String, Identity
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import mapped_column
 from .base import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, Identity(), primary_key=True)
+    id = mapped_column(Integer, primary_key=True)
     username = Column(String(255), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
