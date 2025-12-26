2226d1616c10f2b9c33f47f5e2c61e791b785af1
Aizen-9000 <majianupam173@gmail.com>
2025-12-26 12:27:45 +0530

    Power

--- app/db/models.py
+++ app/db/models.py
@@ -4,12 +4,6 @@ from .base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(
-        Integer,
-        Identity(always=True),
-        primary_key=True,
-        index=True
-    )
-
-    username = Column(String(255), unique=True, index=True, nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
+    id = Column(Integer, Identity(), primary_key=True)
+    username = Column(String(255), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
