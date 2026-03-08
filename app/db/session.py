8ae470de93ef7b63dec8fe2c1abcd14184ea5150
Aizen-9000 <majianupam173@gmail.com>
2026-03-08 10:26:07 +0530

    ohio

--- app/db/session.py
+++ app/db/session.py
@@ -5,8 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from app.config import get_settings
 
 settings = get_settings()
-
-engine = create_engine(settings.DATABASE_URL, future=True)
+engine = create_engine(settings.DATABASE_URL, future=True,   pool_pre_ping=True,  # << automatically checks if connection is alive
+    pool_size=10,        # adjust based on your needs
+    max_overflow=5,)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
