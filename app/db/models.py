46d7204801f093743062516e6b48c93239281553
Aizen-9000 <majianupam173@gmail.com>
2026-03-08 08:43:10 +0530

    Absolute Zero

--- app/db/models.py
+++ app/db/models.py
@@ -5,6 +5,7 @@ from app.db.base import Base
 class User(Base):
     __tablename__ = "users"
 
+    username = Column(String, unique=True, index=True)
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
